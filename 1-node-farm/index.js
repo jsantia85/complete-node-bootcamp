935ce454149eb4bd936c2046001e5096d0356f37
JASantia85 <JuanASantiago1058@gmail.com>
2022-09-20 21:08:46 -0400

    write data

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,5 +1,11 @@
 const fs = require('fs')
 
-const hello = 'hello'
+const textIn = fs.readFileSync('1-node-farm/starter/txt/input.txt', 'utf-8')
 
-console.log(hello)
+console.log(textIn)
+
+const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}.`
+
+fs.writeFileSync('1-node-farm/starter/txt/output.txt', textOut)
+
+console.log('File written!')
